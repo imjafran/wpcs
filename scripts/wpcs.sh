4eeedca7b248bec8755b1e7bfce3ab861cc196dc
Jafran Hasan <exjafran@gmail.com>
2023-01-30 22:29:32 +0600

    updated v1

--- scripts/wpcs.sh
+++ scripts/wpcs.sh
@@ -6,14 +6,12 @@ sudo mv phpcs.phar /usr/local/bin/phpcs
 # Installing WPCS
 wget https://github.com/WordPress/WordPress-Coding-Standards/archive/master.zip
 unzip master.zip
-mv WordPress-Coding-Standards-master ~/wpcs
 
 # Installing PHPCompatibility
 composer require phpcompatibility/php-compatibility
-mv vendor/phpcompatibility/php-compatibility ~/PHPCompatibility
 
 # Setting up paths
-phpcs --config-set installed_paths /home/$USER/wpcs,/home/$USER/PHPCompatibility
+phpcs --config-set installed_paths ./WordPress-Coding-Standards-master,./vendor/phpcompatibility/php-compatibility
 phpcs -i
 
 rm phpcs.xml
